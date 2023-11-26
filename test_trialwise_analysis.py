a4b14df1f5ec68834904e40478a55abe2c8b631d
dsvedberg <dan.ake.svedberg@gmail.com>
2023-11-26 12:58:44 -0600

    nonlinear regression program

---
+++ test_trialwise_analysis.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 13:46:14 2023
+
+@author: danielsvedberg
+"""
+from trialwise_analysis import *
+
+# Generate the synthetic data in long-form
+synthetic_data = generate_synthetic_data()
+synthetic_data.head()  # Displaying the first few rows of the generated data
+
+# Perform nonlinear regression on the synthetic data
+fitted_parameters, r2 = nonlinear_regression(synthetic_data)
+
+# generate datapoints from model for plotting
+fitted_data = generate_fitted_data(fitted_parameters)
+
+#plot scatterplot with fit for each group
+fig, ax = plt.subplots(figsize=(5, 5))
+sns.scatterplot(data=synthetic_data, x='Time', y='Value', hue='Subject', legend=False, ax=ax)
+sns.lineplot(data=fitted_data, x='Time', y='Value', hue='Subject', legend=False, ax=ax)
+plt.show()
+
+#now generate the metafit, which is the average of each individual's fit
+metaparams, metar2, meanr2 = nonlinear_metaregression(synthetic_data)
+metafit = generate_metafit(metaparams,time_steps=30)
+
+
+#plot the metafit with the 95% CI around it
+fig, ax = plt.subplots(figsize=(5, 5))
+sns.scatterplot(data=synthetic_data, x='Time', y='Value', hue='Subject', legend=False)
+ax.plot(metafit.Time, metafit.model_mean, color = 'black')
+ax.fill_between(metafit.Time, metafit.model_high, metafit.model_low, color = 'gray', alpha = 0.5)
+plt.show()
+
+#generate the null distribution of the metafit r2
+shuffdf = iter_shuffle(synthetic_data, niter=2000)
+
+metar2ci = np.percentile(shuffdf.metaR2, [2.5, 97.5])
+meanr2ci = np.percentile(shuffdf.r2_est, [2.5, 97.5])
+
+#plot the null distribution, and the r2 value of the 
+counts, bins = np.histogram(shuffdf.r2_est, bins = 50, range = (-1, 1))
+mask = (shuffdf.r2_est >= meanr2ci[0]) & (shuffdf.r2_est <= meanr2ci[1])
+fig, ax = plt.subplots(figsize=(5, 5), sharey=True)
+ax.hist(shuffdf.r2_est[mask], bins)
+ax.hist(shuffdf.r2_est, bins, histtype = 'step', color='black')
+ax.axvline(x=meanr2['bootmean'])
+plt.show()
+
+
+shuffdf['metaR2'] = np.round(shuffdf['metaR2'], 3)
+counts, bins = np.histogram(shuffdf.metaR2, bins = 50, range = (-1, 1))
+mask = (shuffdf.metaR2 >= metar2ci[0]) & (shuffdf.metaR2 <= metar2ci[1])
+fig, ax = plt.subplots(figsize=(5,5))
+ax.hist(shuffdf.metaR2[mask], bins)
+ax.hist(shuffdf.metaR2, bins, histtype='step', color='black')
+ax.axvline(x=metar2)
+plt.show()
--- trialwise_analysis.py
+++ trialwise_analysis.py
@@ -1,7 +1,12 @@
 import numpy as np
 from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score
 import pandas as pd
-import numpyrho
+from scipy.stats import sem
+from itertools import product
+import seaborn as sns
+import matplotlib.pyplot as plt
+import random
 
 def model(x, a, b, c):
     return a / (1 + np.exp(-b * x + c))
@@ -47,6 +52,17 @@ def generate_synthetic_data(time_steps=30, subjects=3, jitter_strength=0.1):
     longform_data = pd.DataFrame(data)
 
     return longform_data
+
+def shuffle_time(df):
+    newdf = []
+    for name, group in df.groupby('Subject'):
+        nt = list(group['Time'])
+        random.shuffle(nt)
+        group['Time'] = nt
+        newdf.append(group)
+    newdf = pd.concat(newdf)
+    return newdf 
+
 def nonlinear_regression(data):
     """
     Performs nonlinear regression on the synthetic data for each subject.
@@ -62,26 +78,96 @@ def nonlinear_regression(data):
 
     # Initialize a dictionary to store results
     fitted_params = {}
+    r2_scores = {}
 
     # Get unique subjects
     subjects = data['Subject'].unique()
 
     # Fit the model for each subject
-    for subject in subjects:
-        # Filter data for the subject
-        subject_data = data[data['Subject'] == subject]
+    for subject, subject_data in data.groupby('Subject'):
 
         # Extract time and values
         time = subject_data['Time']
         values = subject_data['Value']
 
         # Fit the model
-        params, _ = curve_fit(model, time, values)
+        params, _ = curve_fit(model, time, values, bounds=([0,0,0],'inf'), maxfev=10000)
 
         # Store the fitted parameters
         fitted_params[subject] = params
+        
+        y_pred = model(subject_data['Time'], *params)
+        r2 = r2_score(subject_data['Value'], y_pred)
+        
+        r2_scores[subject] = r2
 
-    return fitted_params
+    return fitted_params, r2_scores
+
+def bootstrap_stats(matrix, n_bootstraps=1000, axis=0):
+    # Number of rows and columns in the matrix
+    n_rows, n_cols = matrix.shape
+
+
+    # Initialize arrays to store bootstrap means and standard deviations
+    boot_means = np.zeros((n_bootstraps, n_cols))
+
+    # Perform bootstrap resampling
+    for i in range(n_bootstraps):
+        # Sample with replacement from the rows of the matrix
+        sample = matrix[np.random.randint(0, n_rows, size=n_rows)]
+        
+        # Calculate mean and standard deviation for each column in the sample
+        boot_means[i, :] = np.mean(sample, axis= axis)
+
+    bootmean = np.mean(boot_means, axis = axis)
+    bootci = np.percentile(boot_means, [2.5, 97.5], axis = axis)
+    
+    params = {'bootmean': bootmean, 'bootci': bootci}
+    return params
+
+def nonlinear_metaregression(data):
+    fitted_params, r2 = nonlinear_regression(data)
+    matrix = np.array(list(fitted_params.values()))
+    metaparams = bootstrap_stats(matrix, n_bootstraps=1000)
+    r2arr = np.array(list(r2.values()))
+    r2arr = r2arr.reshape(-1,1)
+    meanr2 = bootstrap_stats(r2arr, axis=0)
+    
+    y_pred = model(data['Time'], *metaparams['bootmean'])
+    metar2 = r2_score(data['Value'], y_pred)
+    
+    return metaparams, metar2, meanr2
+
+def generate_metafit(metaparams, time_steps = 30):
+    t = np.linspace(0, time_steps-1, time_steps)
+
+    # Create an empty list to store data
+    data = []
+    
+    meanparams = metaparams['bootmean']
+    matrix = metaparams['bootci']
+    
+    # Generate all combinations of column entries
+    transposed_matrix = matrix.T
+    column_combinations = list(product(*transposed_matrix))
+    
+    res = []
+    for row in column_combinations:
+        ci_data = model(t, *row)
+        res.append(ci_data)
+    res = np.vstack(res)
+    est_high = np.max(res,axis=0)
+    est_low = np.min(res,axis=0)
+    
+    mean_data = model(t, *meanparams)
+    # Append to data list
+    
+    df = pd.DataFrame({'Time':t,
+                       'model_mean':mean_data,
+                       'model_high': est_high,
+                       'model_low': est_low})
+    return df
+    
 
 #make a dataframe modeling the points that would be put out by the models in fitted parameters
 def generate_fitted_data(fitted_parameters, time_steps=30):
@@ -121,20 +207,40 @@ def generate_fitted_data(fitted_parameters, time_steps=30):
 
     return longform_data
 
-# Generate the synthetic data in long-form
-synthetic_data = generate_synthetic_data()
-synthetic_data.head()  # Displaying the first few rows of the generated data
+def iter_shuffle(data, niter = 10000):
+    iters = []
+    params = []
+    param_high = []
+    param_low = []
+    for i in range(niter): 
+        shuff = shuffle_time(data)
+        metaparams, metar2, meanr2 = nonlinear_metaregression(shuff)
+        paramest = metaparams['bootmean']
+        metaci = metaparams['bootci']
+        paramlow = metaci[0,:]
+        paramhigh = metaci[1,:]
+        r2est = meanr2['bootmean']
+        r2ci = meanr2['bootci']
+        r2low = r2ci[0,:]
+        r2high = r2ci[1,:] 
+        
+        datadict = {'iternum': i,
+                    'metaR2' : metar2,
+                    'r2_est' : r2est,
+                    'r2_low' : r2low,
+                    'r2_high': r2high}
+        df = pd.DataFrame(datadict)
+        iters.append(df)
+        params.append(paramest)
+        param_low.append(paramlow)
+        param_high.append(paramhigh)
 
-# Perform nonlinear regression on the synthetic data
-fitted_parameters = nonlinear_regression(synthetic_data)
-
-
-fitted_data = generate_fitted_data(fitted_parameters)
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots(figsize=(5, 5))
-p = sns.scatterplot(data=synthetic_data, x='Time', y='Value', hue='Subject', legend=False)
-p2 = sns.lineplot(data=fitted_data, x='Time', y='Value', hue='Subject', legend=False, ax=ax)
-plt.show()
-
+        
+    iters = pd.concat(iters)
+    iters['param_est'] = params
+    iters['param_low'] = param_low
+    iters['param_high'] = param_high        
+    
+    return iters
+        
+        
